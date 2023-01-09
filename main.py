6c56d39ae8ab8256e8c42533fadda7ca827d3cbe
Luai Al-Tajar <luaialtajar1@gmail.com>
2023-01-09 18:15:40 +0200

    added sticky to buttons

--- main.py
+++ main.py
@@ -20,41 +20,41 @@ class Calculator:
 
         # Create the display area
         self.display = tk.Entry(self.root, font=("Arial", 20), justify="right")
-        self.display.grid(row=0, column=0, columnspan=4, sticky="ew")
+        self.display.grid(row=0, column=0, columnspan=4)
 
         # Create the buttons
         tk.Button(self.root, text="1", font=("Arial", 20),
-                  command=lambda: self.enter_number("1")).grid(row=3, column=0)
+                  command=lambda: self.enter_number("1")).grid(row=3, column=0, sticky="ew")
         tk.Button(self.root, text="2", font=("Arial", 20),
-                  command=lambda: self.enter_number("2")).grid(row=3, column=1)
+                  command=lambda: self.enter_number("2")).grid(row=3, column=1, sticky="ew")
         tk.Button(self.root, text="3", font=("Arial", 20),
-                  command=lambda: self.enter_number("3")).grid(row=3, column=2)
+                  command=lambda: self.enter_number("3")).grid(row=3, column=2, sticky="ew")
         tk.Button(self.root, text="4", font=("Arial", 20),
-                  command=lambda: self.enter_number("4")).grid(row=2, column=0)
+                  command=lambda: self.enter_number("4")).grid(row=2, column=0, sticky="ew")
         tk.Button(self.root, text="5", font=("Arial", 20),
-                  command=lambda: self.enter_number("5")).grid(row=2, column=1)
+                  command=lambda: self.enter_number("5")).grid(row=2, column=1, sticky="ew")
         tk.Button(self.root, text="6", font=("Arial", 20),
-                  command=lambda: self.enter_number("6")).grid(row=2, column=2)
+                  command=lambda: self.enter_number("6")).grid(row=2, column=2, sticky="ew")
         tk.Button(self.root, text="7", font=("Arial", 20),
-                  command=lambda: self.enter_number("7")).grid(row=1, column=0)
+                  command=lambda: self.enter_number("7")).grid(row=1, column=0, sticky="ew")
         tk.Button(self.root, text="8", font=("Arial", 20),
-                  command=lambda: self.enter_number("8")).grid(row=1, column=1)
+                  command=lambda: self.enter_number("8")).grid(row=1, column=1, sticky="ew")
         tk.Button(self.root, text="9", font=("Arial", 20),
-                  command=lambda: self.enter_number("9")).grid(row=1, column=2)
+                  command=lambda: self.enter_number("9")).grid(row=1, column=2, sticky="ew")
         tk.Button(self.root, text="0", font=("Arial", 20),
-                  command=lambda: self.enter_number("0")).grid(row=4, column=1)
+                  command=lambda: self.enter_number("0")).grid(row=4, column=1, sticky="ew")
         tk.Button(self.root, text="/", font=("Arial", 20),
-                  command=lambda: self.enter_operation("/")).grid(row=2, column=3)
+                  command=lambda: self.enter_operation("/")).grid(row=2, column=3, sticky="ew")
         tk.Button(self.root, text="*", font=("Arial", 20),
-                  command=lambda: self.enter_operation("*")).grid(row=3, column=3)
+                  command=lambda: self.enter_operation("*")).grid(row=3, column=3, sticky="ew")
         tk.Button(self.root, text="+", font=("Arial", 20),
-                  command=lambda: self.enter_operation("+")).grid(row=4, column=0)
+                  command=lambda: self.enter_operation("+")).grid(row=4, column=0, sticky="ew")
         tk.Button(self.root, text="-", font=("Arial", 20),
-                  command=lambda: self.enter_operation("-")).grid(row=4, column=2)
+                  command=lambda: self.enter_operation("-")).grid(row=4, column=2, sticky="ew")
         tk.Button(self.root, text="=", font=("Arial", 20),
-                  command=lambda: self.enter_operation("=")).grid(row=4, column=3)
+                  command=lambda: self.enter_operation("=")).grid(row=4, column=3, sticky="ew")
         tk.Button(self.root, text="C", font=("Arial", 20),
-                  command=lambda: self.clear()).grid(row=1, column=3)
+                  command=lambda: self.clear()).grid(row=1, column=3, sticky="ew")
 
         # Start the event loop
         self.root.mainloop()
