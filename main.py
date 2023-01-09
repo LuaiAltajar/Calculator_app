672fa9caae3cead768e4c22e6a241ce3a5c2e16f
Luai Al-Tajar <luaialtajar1@gmail.com>
2023-01-09 17:27:37 +0200

    added comments

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ import tkinter as tk
 
 class Calculator:
     def __init__(self):
-        self.operation = ""
+        """
+        Creates the root window, sets the title and dimensions and creates the display and buttons.
+        """
+        # Create the root window
         self.root = tk.Tk()
+
+        # Set the window title
         self.root.title("Calculator")
+
+        # Disable resizing of the window
         self.root.resizable(False, False)
 
+        # Initialize an empty string to store the current operation
+        self.operation = ""
+
+        # Create the display area
         self.display = tk.Entry(self.root, font=("Arial", 20), justify="right")
         self.display.grid(row=0, column=0, columnspan=4)
 
+        # Create the buttons
         tk.Button(self.root, text="1", font=("Arial", 20),
                   command=lambda: self.enter_number("1")).grid(row=3, column=0, sticky="ew")
         tk.Button(self.root, text="2", font=("Arial", 20),
@@ -43,21 +55,40 @@ class Calculator:
                   command=lambda: self.enter_operation("=")).grid(row=4, column=3, sticky="ew")
         tk.Button(self.root, text="C", font=("Arial", 20),
                   command=lambda: self.clear()).grid(row=1, column=3, sticky="ew")
+
+        # Start the event loop
         self.root.mainloop()
 
     def enter_number(self, number):
+        """
+        Update the display with the entered number.
+        :param: The number to be entered.
+        :return: None
+        """
         self.display.insert(tk.END, number)
 
     def enter_operation(self, operator):
+        """
+        Perform the specified operation and update the display.
+        :param: The operation to be performed.
+        :return: None
+        """
         if operator == "=":
+            # Evaluate the expression in the display
             self.operation = str(eval(self.display.get()))
+
+            # Clear the display
             self.display.delete(0, tk.END)
+
+            # Insert the result
             self.display.insert(0, self.operation)
         else:
             self.operation = operator
+            # Update the display with the operator
             self.display.insert(tk.END, operator)
 
     def clear(self):
+        # Clear the contents of the display.
         self.display.delete(0, tk.END)
 
 
